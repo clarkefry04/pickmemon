57392da9ca0cf86d9871e97a98f1300e2495532a
Clarke Fry <clarkefry@gmail.com>
2018-01-24 20:33:46 -0600

    updated index.js

--- app/models/index.js
+++ app/models/index.js
@@ -1,13 +1,12 @@
-"use strict";
- 
-var fs = require("fs");
-var path = require("path");
-var Sequelize = require("sequelize");
+'use strict';
+
+var fs        = require('fs');
+var path      = require('path');
+var Sequelize = require('sequelize');
 var basename  = path.basename(module.filename);
-var env = process.env.NODE_ENV || "development";
+var env       = process.env.NODE_ENV || 'development';
 var config    = require(__dirname + '/../config/config.json')[env];
-var sequelize = new Sequelize(config.database, config.username, config.password, config);
-var db = {};
+var db        = {};
 
 if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env[config.use_env_variable]);
